2afda3806e6b9f6091e59f7a019dde92180a7764
BCPalmer <bryce.palmer@my.uwrf.edu>
2015-12-10 20:43:26 -0600

    Added placeholder files for history page

---
+++ app/src/main/java/com/example/paul/turbulentwaffle/HistoryScreen.java
@@ -0,0 +1,14 @@
+package com.example.paul.turbulentwaffle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class HistoryScreen {
+}
