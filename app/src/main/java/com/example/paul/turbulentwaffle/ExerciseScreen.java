4ab2f185aefcf970b4e34cebe3c6563e59d165a1
AndrewJOwens <andrew.owens@my.uwrf.edu>
2015-12-20 20:54:16 -0600

    Merge branch 'master' of C:\Users\Yam\AndroidStudioProjects\TurbulentWaffle with conflicts.

--- app/src/main/java/com/example/paul/turbulentwaffle/ExerciseScreen.java
+++ app/src/main/java/com/example/paul/turbulentwaffle/ExerciseScreen.java
@@ -12,31 +12,35 @@ import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  * Created by Yam on 12/10/2015.
 */
-public class ExerciseScreen extends Activity{
+public class ExerciseScreen extends Activity {
 
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor sharedEditor;
-    private ArrayList<activitiesListItem>   activityList = importActivityList(),
-                                            sortedList;
+    private ArrayList<activitiesListItem> activityList ,
+            sortedList;
+    private ArrayList<String> menuList;
     private activitiesListItem selectedActivity;
     private Spinner type_Spinner,
-                    activity_Spinner;
-                    //MET_Spinner;
-    private String  selectActivity_string,
-                    typeSelected,
-                    activitySelected,
-                    calsBurned,
-                    timeSpent_string;
+            activity_Spinner;
+    //MET_Spinner;
+    private String selectActivity_string,
+            typeSelected,
+            activitySelected,
+            calsBurned,
+            timeSpent_string;
     private EditText timeSpent_EditText;
 
-@Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.exercise_layout);
 
@@ -44,97 +48,130 @@ public class ExerciseScreen extends Activity{
                 getString(R.string.PREF_FILE), MODE_PRIVATE);
         sharedEditor = sharedPreferences.edit();
 
-    createSpinners();
+        createSpinners();
         createEditTexts();
+        importActivityList();
         addItemToTypeSpinner();
+        addItemToActivitySpinner();
         addListenerToSpinners();
     }
     public void createSpinners() {
         type_Spinner=(Spinner)findViewById(R.id.type_spinner_id);
         activity_Spinner=(Spinner)findViewById(R.id.activity_spinner_id);
-        activity_Spinner.setEnabled(false);
         //MET_Spinner=(Spinner)findViewById(R.id.MET_spinner_id);
     }
     public void createEditTexts() {
         timeSpent_EditText=(EditText)findViewById(R.id.timeSpent_editText_id);
     }
     public void addItemToTypeSpinner() {
-        ArrayAdapter<CharSequence> type_Adapter = ArrayAdapter.createFromResource(this,
-                R.array.activityTypes,
-                android.R.layout.simple_spinner_item);
-        type_Adapter.setDropDownViewResource(
+        ArrayAdapter<CharSequence> typeSpinnerAdapter = //Set the serving spinners to go between
+                ArrayAdapter.createFromResource(this,     //the various numbers
+                        R.array.activity_data,
+                        android.R.layout.simple_spinner_item);
+        typeSpinnerAdapter.setDropDownViewResource(
                 android.R.layout.simple_spinner_dropdown_item);
 
-        type_Spinner.setAdapter(type_Adapter);
+        type_Spinner.setAdapter(typeSpinnerAdapter);
+
     }
+    public void addItemToActivitySpinner() {
+     ArrayList<String> tempList = new ArrayList<>();
+        tempList.add("Please select type first");
 
-
-    public void addItemToActivitySpinner(String selectedType) {
-        ArrayList<String> tempList = new ArrayList<>();
-        for (int x = 0; x < activityList.size(); x++) {
-            if (activityList.get(x).getType().equals(selectedType)) {
-                sortedList.add(activityList.get(x));
-            }
-        }
-        for (int x = 0; x < sortedList.size(); x++) {
-            tempList.add(sortedList.get(x).getActivity());
-        }
         ArrayAdapter<String> activity_Adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, tempList);
         activity_Adapter.setDropDownViewResource(
                 android.R.layout.simple_spinner_dropdown_item);
 
         activity_Spinner.setAdapter(activity_Adapter);
+
     }
+
+
+    public void UpdateActivitySpinner(View view) {
+
+
+            ArrayList<String> tempList = new ArrayList<>();
+            for (int x = 0; x < activityList.size(); x++) {
+                if (activityList.get(x).getType().equals(activitySelected)) {
+                    sortedList.add(activityList.get(x));
+                }
+            }
+            for (int x = 0; x < sortedList.size(); x++) {
+                tempList.add(sortedList.get(x).getActivity());
+                //System.out.println(sortedList.get(x).getActivity());
+            }
+
+            ArrayAdapter<String> activity_Adapter = new ArrayAdapter<>(this,
+                    android.R.layout.simple_spinner_item, tempList);
+            activity_Adapter.setDropDownViewResource(
+                    android.R.layout.simple_spinner_dropdown_item);
+
+
+           //activity_Spinner.setAdapter(activity_Adapter);
+    }
+
     public void addListenerToSpinners() {
         type_Spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 typeSelected = parent.getItemAtPosition(position).toString();
-                addItemToActivitySpinner(typeSelected);
-                activity_Spinner.setEnabled(true);
+
             }
 
             public void onNothingSelected(AdapterView<?> parent) {/*WHAT DID YOU DO?????*/}
+
         });
+
         activity_Spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 activitySelected = parent.getItemAtPosition(position).toString();
-                getFinalActivity(activitySelected);
+                UpdateActivitySpinner(view);
             }
-            public void onNothingSelected(AdapterView<?> parent) {/*WHAT DID YOU DO?????*/}
+            public void onNothingSelected(AdapterView<?> parent) {/*WHAT DID YOU DO?????*/
+
+        }
         });
     }
-
     //Import the activity data from the activityData text file
-    public ArrayList<activitiesListItem> importActivityList() {
+    public void importActivityList() {
         ArrayList<activitiesListItem> activityArray = new ArrayList<>();
-        ArrayList<activitiesListItem> sortedArray = new ArrayList<>();
         Scanner grabber = null;
-        String iType = null;
-        String iActivity = null;
-        double iMET = 0;
+        String iType;
+        String iActivity;
+        double iMET;
+        System.out.println("Test");
 
-        try {
-            grabber = new Scanner(new FileReader("activityData"));
-        } catch (FileNotFoundException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
+
+            try {
+                grabber = new Scanner(getAssets().open("activitiesData"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
+        if (grabber != null) {
+            grabber.useDelimiter(";|\r\n");
+            do {
+
+                iType = grabber.next();
+
+                iActivity = grabber.next();
+                iMET = grabber.nextDouble();
+
+                activitiesListItem temp = new activitiesListItem(iType, iActivity, iMET);
+                activityArray.add(temp);
+            } while (grabber.hasNext()) ;
         }
-        grabber.useDelimiter(";|\r\n");
-        do {
-            iType = grabber.next();
-            iActivity = grabber.next();
-            iMET = grabber.nextDouble();
 
-            activitiesListItem temp = new activitiesListItem(iType, iActivity, iMET );
-            activityArray.add(temp);
-        } while (grabber.hasNext());
+        activityList = activityArray;
 
-
-        return activityArray;
+        
     }
+
+
     public void getFinalActivity(String selcectedActivityName) {
         for(int x = 0; x < sortedList.size(); x++) {
             if (sortedList.get(x).getActivity().equals(selcectedActivityName)) {
@@ -143,13 +180,14 @@ public class ExerciseScreen extends Activity{
         }
     }
 
-    public void onCalculate() {
+
+    public void onCalculate(View view) {
         boolean passChecker = true;
         if (timeSpent_EditText.getText().toString().isEmpty()) {
             passChecker = false;
         }
         if (!passChecker) {
-            Toast.makeText(this, "Please enter missing field", Toast.LENGTH_SHORT);
+            Toast.makeText(this, "Please enter missing field", Toast.LENGTH_SHORT).show();
         } else {
             calsBurned = Double.toString(selectedActivity.getMET() * R.string.USER_WEIGHT *
                     Double.parseDouble(timeSpent_string));
